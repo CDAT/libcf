fa15da9e63980536777de34cb35f21eeb722a503
Denis Nadeau <dnadeau4@users.noreply.github.com>
2017-03-09 16:09:10 -0800

    add CFLAGS

--- scripts/circleci_mac.sh
+++ scripts/circleci_mac.sh
@@ -4,9 +4,11 @@ echo "CI_PULL_REQUESTS"$CI_PULL_REQUESTS
 echo "CI_PULL_REQUEST"$CI_PULL_REQUEST
 
 export CXXFLAGS="${CXXFLAGS} -fno-common"
+export CFLAGS="${XFLAGS} -fno-common"
 export MACOSX_DEPLOYMENT_TARGET=$(sw_vers -productVersion | sed -E "s/([0-9]+\.[0-9]+).*/\1/")
 export DYLD_FALLBACK_LIBRARY_PATH=${PREFIX}/lib
 export LDFLAGS="${LDFLAGS} -lpython" 
+env
 
 pwd
 ./configure --prefix=${HOME}/miniconda2
