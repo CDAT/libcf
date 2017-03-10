f53f8f69b1128cf36555b1349441959e7bc3f5b2
Charles Doutriaux <doutriaux1@llnl.gov>
2017-03-09 21:30:19 -0800

    correct prfix value miniconda not miniconda2

--- scripts/circleci_mac.sh
+++ scripts/circleci_mac.sh
@@ -3,7 +3,7 @@ echo "CIRCLE CI BRANCH:"$CIRCLE_BRANCH
 echo "CI_PULL_REQUESTS"$CI_PULL_REQUESTS
 echo "CI_PULL_REQUEST"$CI_PULL_REQUEST
 
-export PREFIX=${HOME}/miniconda2
+export PREFIX=${HOME}/miniconda
 export CXXFLAGS="${CXXFLAGS} -fno-common"
 export CFLAGS="${XFLAGS} -fno-common"
 #export MACOSX_DEPLOYMENT_TARGET=$(sw_vers -productVersion | sed -E "s/([0-9]+\.[0-9]+).*/\1/")
