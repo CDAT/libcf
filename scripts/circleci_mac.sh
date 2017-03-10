598072fe9dd7b16f3feeb15400556bd1f8b19ece
Charles Doutriaux <doutriaux1@llnl.gov>
2017-03-09 21:29:04 -0800

    trying to set LDFLAGS

--- scripts/circleci_mac.sh
+++ scripts/circleci_mac.sh
@@ -3,16 +3,18 @@ echo "CIRCLE CI BRANCH:"$CIRCLE_BRANCH
 echo "CI_PULL_REQUESTS"$CI_PULL_REQUESTS
 echo "CI_PULL_REQUEST"$CI_PULL_REQUEST
 
+export PREFIX=${HOME}/miniconda2
 export CXXFLAGS="${CXXFLAGS} -fno-common"
 export CFLAGS="${XFLAGS} -fno-common"
-export MACOSX_DEPLOYMENT_TARGET=$(sw_vers -productVersion | sed -E "s/([0-9]+\.[0-9]+).*/\1/")
-export DYLD_FALLBACK_LIBRARY_PATH=${PREFIX}/lib
-export DYLD_LIBRARY_PATH=${PREFIX}/lib
-export LDFLAGS="${LDFLAGS} -lpython" 
+#export MACOSX_DEPLOYMENT_TARGET=$(sw_vers -productVersion | sed -E "s/([0-9]+\.[0-9]+).*/\1/")
+#export DYLD_FALLBACK_LIBRARY_PATH=${PREFIX}/lib
+#export DYLD_LIBRARY_PATH=${PREFIX}/lib
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -lpython " 
+export LFLAGS="-fPIC"
 env
 
 pwd
-env DYLD_LIBRARY_PATH=${PREFIX}/lib ./configure --prefix=${HOME}/miniconda2
+./configure --prefix=${PREFIX}
 make
 make install
 
