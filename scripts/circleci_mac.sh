b9c54124a98dc854c0747a85255a35dfbf8696cd
Charles Doutriaux <doutriaux1@llnl.gov>
2017-03-09 21:41:40 -0800

    no need for fortran

--- scripts/circleci_mac.sh
+++ scripts/circleci_mac.sh
@@ -6,11 +6,9 @@ echo "CI_PULL_REQUEST"$CI_PULL_REQUEST
 export PREFIX=${HOME}/miniconda
 export CXXFLAGS="${CXXFLAGS} -fno-common"
 export CFLAGS="${XFLAGS} -fno-common"
-#export MACOSX_DEPLOYMENT_TARGET=$(sw_vers -productVersion | sed -E "s/([0-9]+\.[0-9]+).*/\1/")
-#export DYLD_FALLBACK_LIBRARY_PATH=${PREFIX}/lib
-#export DYLD_LIBRARY_PATH=${PREFIX}/lib
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -lpython " 
-export LFLAGS="-fPIC"
+# somehow looks for fortran but doesn't seem to need it it's pure C
+# looking for fortran makes mac fail
+export FC="" 
 env
 
 pwd
