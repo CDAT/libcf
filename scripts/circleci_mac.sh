f63be7df21c6d4ef6eab8873733da2d38d40a590
Denis Nadeau <dnadeau4@users.noreply.github.com>
2017-03-09 16:03:43 -0800

    add some env variables

--- scripts/circleci_mac.sh
+++ scripts/circleci_mac.sh
@@ -3,6 +3,11 @@ echo "CIRCLE CI BRANCH:"$CIRCLE_BRANCH
 echo "CI_PULL_REQUESTS"$CI_PULL_REQUESTS
 echo "CI_PULL_REQUEST"$CI_PULL_REQUEST
 
+export CXXFLAGS="${CXXFLAGS} -fno-common"
+export MACOSX_DEPLOYMENT_TARGET=$(sw_vers -productVersion | sed -E "s/([0-9]+\.[0-9]+).*/\1/")
+export DYLD_FALLBACK_LIBRARY_PATH=${PREFIX}/lib
+export LDFLAGS="${LDFLAGS} -lpython" 
+
 pwd
 ./configure --prefix=${HOME}/miniconda2
 make
