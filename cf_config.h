0b550dc35a8a88966edb189428f3ae459b355c52
Denis Nadeau <dnadeau4@users.noreply.github.com>
2018-05-08 18:11:58 -0700

    udpate libcf to py3

--- cf_config.h
+++ cf_config.h
@@ -21,10 +21,10 @@
 
 /* Define to a macro mangling the given C identifier (in lower and upper
    case), which must not contain underscores, for linking with Fortran. */
-#define FC_FUNC(name,NAME) name ## _
+/* #undef FC_FUNC */
 
 /* As FC_FUNC, but for C identifiers containing underscores. */
-#define FC_FUNC_(name,NAME) name ## _
+/* #undef FC_FUNC_ */
 
 /* Define to 1 if you have `alloca', as a function or macro. */
 #define HAVE_ALLOCA 1
@@ -52,7 +52,7 @@
 #define HAVE_MEMORY_H 1
 
 /* Define to 1 if you have the <mpi.h> header file. */
-/* #undef HAVE_MPI_H */
+#define HAVE_MPI_H 1
 
 /* Define to 1 if you have the <netcdf.h> header file. */
 #define HAVE_NETCDF_H 1
@@ -70,7 +70,7 @@
 #define HAVE_STRING_H 1
 
 /* Define this if you have strlcat() */
-#define HAVE_STRLCAT 1
+/* #undef HAVE_STRLCAT */
 
 /* Define to 1 if `st_blksize' is a member of `struct stat'. */
 #define HAVE_STRUCT_STAT_ST_BLKSIZE 1
@@ -120,7 +120,7 @@
 #define PACKAGE_NAME "libcf"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "libcf 1.0-beta11"
+#define PACKAGE_STRING "libcf 1.0-beta12"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "libcf"
@@ -129,7 +129,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.0-beta11"
+#define PACKAGE_VERSION "1.0-beta12"
 
 /* The size of `double', as computed by sizeof. */
 #define SIZEOF_DOUBLE 8
@@ -164,7 +164,7 @@
 /* #undef USE_PARALLEL */
 
 /* Version number of package */
-#define VERSION "1.0-beta11"
+#define VERSION "1.0-beta12"
 
 /* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
    significant byte first (like Motorola and SPARC, unlike Intel). */
