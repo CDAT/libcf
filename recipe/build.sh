ddd614b48192918c34f695ce43cd5777e2a6f11c
Denis Nadeau <dnadeau4@users.noreply.github.com>
2019-07-05 15:03:14 -0700

    Fixdependencies (#11)
    * add libcblas and libblas to meta.yaml
    
    * recompile libcf with new libblas and libcblas
    
    * change compiler to clang
    
    * add CFLAGS for osx64 in build
    
    * fix CPPFLAGS and CFLAGS for clang

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,15 @@
-#source activate "${CONDA_DEFAULT_ENV}"
-export CFLAGS="-Wall -m64 -pipe -O2  -fPIC ${CFLAGS}"
+export CFLAGS="-Wall -m64 -pipe -O2  -fPIC -I${PREFIX}/include ${CFLAGS}"
 export CXXFLAGS="${CFLAGS} ${CXXFLAGS}"
 export CPPFLAGS="-I${PREFIX}/include ${CPPFLAGS}"
 export LDFLAGS="-L${PREFIX}/lib ${LDFLAGS}"
 export LFLAGS="-fPIC ${LFLAGS}"
 export FC=""
+# needed for clang_osx-64
+if [ ${HOME} == "/Users/distiller" ]; then
+    export CFLAGS="-Wl,-syslibroot / -isysroot / ${CFLAGS}"
+    # configure need this otherwise "error.h" is not found and configure report netcdf.h 
+    export CPPFLAGS="-Wl,-syslibroot / -isysroot / -I${PREFIX}/include ${CPPFLAGS}"
+fi
 ./configure --prefix=${PREFIX}
 make
 make install
