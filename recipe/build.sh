1763888a1348636c56b5116bf2e95742c85b460c
muryanto1 <muryanto1@llnl.gov>
2019-11-14 13:25:33 -0800

    rerun w/ new conda_build_upload.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,25 +1,22 @@
-#!/bin/sh
 export CFLAGS="-Wall -m64 -pipe -O2  -fPIC -I${PREFIX}/include ${CFLAGS}"
 export CXXFLAGS="${CFLAGS} ${CXXFLAGS}"
 export CPPFLAGS="-I${PREFIX}/include ${CPPFLAGS}"
 export LDFLAGS="-L${PREFIX}/lib ${LDFLAGS}"
 export LFLAGS="-fPIC ${LFLAGS}"
 export FC=""
+
 # needed for clang_osx-64
 if [ ${HOME} == "/Users/distiller" ]; then
     export CFLAGS="-Wl,-syslibroot / -isysroot / ${CFLAGS}"
     # configure need this otherwise "error.h" is not found and configure report netcdf.h 
     export CPPFLAGS="-Wl,-syslibroot / -isysroot / -I${PREFIX}/include ${CPPFLAGS}"
 fi
-echo "XXX XXX"
-ls
-echo "./configure --prefix=${PREFIX}"
 ./configure --prefix=${PREFIX}
 make
 make install
-if [ $(uname) == "Linux" ];then
-    export LDSHARED="$CC -shared -pthread"
-    LDSHARED="$CC -shared -pthread" python setup.py install
+if [ `uname` == Linux ]; then
+    LDSHARED="$CC -shared -pthread"  ${PYTHON} setup.py install;
 else
-    python setup.py install
+    # LDSHARED_FLAGS="-bundle -undefined dynamic_lookup"  ${PYTHON} setup.py install;
+    ${PYTHON} setup.py install;
 fi
